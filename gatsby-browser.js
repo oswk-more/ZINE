03f407739684b0afa2fc4f41f9ed7efea7b27674
oswk-more <oswk.more+dev@gmail.com>
2024-01-28 02:27:28 +0900

    first Commit

---
+++ gatsby-browser.js
@@ -0,0 +1,14 @@
+// custom typefaces
+import "@fontsource-variable/montserrat"
+import "@fontsource/merriweather"
+
+//tailwindcss
+import "./src/styles/global.css"
+
+// normalize CSS across browsers
+import "./src/normalize.css"
+// custom CSS styles
+import "./src/style.css"
+
+// Highlighting for code blocks
+import "prismjs/themes/prism.css"
---
+++ gatsby-config.js
@@ -0,0 +1,126 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ZINE` /*サイトタイトル*/,
+    author: {
+      name: `Sou Oshikawa` /*サイトの運営者*/,
+      summary: `ここに入れる` /*まとめ*/,
+    },
+    description: `` /*サイトの説明欄*/,
+    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/` /*リンク先サイトのURL*/,
+    social: {
+      twitter: `oswk_w` /*Xリンクの末尾*/,
+    },
+  },
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-image` /*画像処理用プラグイン*/,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp` /*画像処理用*/,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-feed` /*さいと*/,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ "content:encoded": node.html }],
+                })
+              })
+            },
+            query: `{
+              allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
+                nodes {
+                  excerpt
+                  html
+                  fields {
+                    slug
+                  }
+                  frontmatter {
+                    title
+                    date
+                  }
+                }
+              }
+            }`,
+            output: "/rss.xml",
+            title: "Gatsby Starter Blog RSS Feed",
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest` /*PWA用*/,
+      options: {
+        name: `ZINE`,
+        short_name: `ZINE`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
